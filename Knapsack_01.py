b219cb9909d0723091b033d1722aa0c14f9c29da
UjwalAgrawal <uvagrawal123@gmail.com>
2020-10-15 19:30:32 +0530

    memoized

--- Knapsack_01.py
+++ Knapsack_01.py
@@ -1,10 +1,18 @@
+dp = {}
+
 def knapsack(weights, values, maxWeight, n):
     if(n==0 or maxWeight==0):
         return(0)
+    if((maxWeight, n) in dp):
+        # print("found it")
+        # It is a hit
+        return(dp[(maxWeight, n)])
     if(weights[n-1]<=maxWeight):
-        return max(values[n-1] + knapsack(weights, values, maxWeight-weights[n-1], n-1), knapsack(weights, values, maxWeight, n-1))
+        dp[(maxWeight, n)] = max(values[n-1] + knapsack(weights, values, maxWeight-weights[n-1], n-1), knapsack(weights, values, maxWeight, n-1))
+        return(dp[(maxWeight, n)])
     else:
-        return(knapsack(weights, values, maxWeight, n-1))
+        dp[(maxWeight, n)] = knapsack(weights, values, maxWeight, n-1)
+        return(dp[(maxWeight, n)])
 
 
 if __name__ == "__main__":
