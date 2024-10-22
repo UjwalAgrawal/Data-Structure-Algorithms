86224c29b656ac06bfaecafead8d16b5acca189d
Ujwal Agrawal <uvagrawal123@gmail.com>
2024-10-21 20:59:29 -0700

    Code update

--- Longest Palindrome.py
+++ Longest Palindrome.py
@@ -1,7 +1,24 @@
-#Given a string S, find the longest palindromic substring in S.
+# Leetcode 5: Given a string S, find the longest palindromic substring in S.
 
 def longestPalindrome(s):
-    pass
+    n = len(s)
+    res = ""
 
-s = input()
-print(longestPalindrome(s))
+    def findPalindrome(left, right):
+        while left > -1 and right < n and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return s[left+1: right]
+
+    for i in range(n):
+        odd = findPalindrome(i, i)
+        even = findPalindrome(i, i+1)
+
+        t = odd if len(odd) > len(even) else even
+        res = t if len(t) > len(res) else res
+    
+    return res
+
+s = 'amalayalam'
+# s = 'cbbd'
+print("Longest palindromic substring is:", longestPalindrome(s))
